4acf483ae2c2581e853315f754e8bd7faed508f5
小雷 <1720402825@qq.com>
2018-03-25 14:13:03 +0800

    add author detail

--- app/src/main/java/com/android/remote/MainActivity.java
+++ app/src/main/java/com/android/remote/MainActivity.java
@@ -3,6 +3,10 @@ package com.android.remote;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author 小雷
+ * @date 2018.3.25
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
